951059af98f52c396bb26060815ad102b4541752
Alex Pearson <apearson@five-startech.com>
2016-07-23 23:35:55 -0500

    Remove modernizr

--- conf/grunt/configurations/copy.js
+++ conf/grunt/configurations/copy.js
@@ -1,20 +1,27 @@
 module.exports = function getCopyConfig( grunt, options ){
     return {
         "dist": {
-            "files": [ {
-                "expand": true,
-                "dot": true,
-                "cwd": options.yeoman.app,
-                "dest": options.yeoman.dist,
-                "src": [
-                    "*.{ico,txt}",
-                    "images/{,*/}*.{webp,gif}",
-                    "styles/fonts/{,*/}*.*",
-                ]
-            }, {
-                "src": "node_modules/apache-server-configs/dist/.htaccess",
-                "dest": `${options.yeoman.dist}/.htaccess`
-            } ]
+            "files": [
+                {
+                    "expand": true,
+                    "dot": true,
+                    "cwd": options.yeoman.app,
+                    "dest": options.yeoman.dist,
+                    "src": [
+                        "*.{ico,txt}",
+                        "images/{,*/}*.{webp,gif}",
+                        "styles/fonts/{,*/}*.*",
+                    ]
+                },
+                {
+                    "src": "node_modules/apache-server-configs/dist/.htaccess",
+                    "dest": `${options.yeoman.dist}/.htaccess`
+                },
+                {
+                    "src": "node_modules/requirejs/require.js",
+                    "dest": `${options.yeoman.dist}/vendor/require.js`
+                }
+            ]
         }
     };
 };
