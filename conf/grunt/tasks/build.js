2ac0124535c9e8ed39598ac880be74c7bab31b7c
Alex Pearson <apearson@five-startech.com>
2016-07-24 14:32:53 -0500

    Activate uglify build step

--- conf/grunt/tasks/build.js
+++ conf/grunt/tasks/build.js
@@ -10,7 +10,7 @@ module.exports = function gruntServe( grunt ){
         "babel:build",
         "requirejs",
         "concat",
-        // "uglify",
+        "uglify",
         "copy",
         "usemin"
     ] );
