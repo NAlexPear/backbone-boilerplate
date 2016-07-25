8d0f264bf2e94ab4be7db73d14d1fd92182f821c
Alex Pearson <apearson@five-startech.com>
2016-07-24 21:44:38 -0500

    Add DELETE route for users

--- server/routes/users/users.js
+++ server/routes/users/users.js
@@ -6,12 +6,14 @@ var router = express.Router();
 var post = require( "./POST/routes.js" );
 var read = require( "./GET/routes.js" );
 var update = require( "./PUT/routes.js" );
+var remove = require( "./DELETE/routes.js" );
 
 router.route( "/" )
     .get( read )
     .post( post );
 
 router.route( "/:user_id" )
-    .put( update );
+    .put( update )
+    .delete( remove );
 
 module.exports = router;
