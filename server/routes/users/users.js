a95087582d3cb1966ea9c15733a3368331c5312b
Alex Pearson <apearson@five-startech.com>
2016-07-24 20:32:25 -0500

    Set up GET route for users

--- server/routes/users/users.js
+++ server/routes/users/users.js
@@ -5,6 +5,7 @@ var express = require( "express" );
 var router = express.Router();
 var _ = require( "underscore" );
 var post = require( "./POST/routes.js" );
+var read = require( "./GET/routes.js" );
 
 
-module.exports = _( router ).extend( post );
+module.exports = _( router ).extend( post, read );
